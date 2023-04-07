53d8bf1d1558af598a1e99679e1cdf6844631468
vajagani dhanesh <vajaganidhanesh@gmail.com>
2023-04-07 13:17:48 +0530

    waitFor is used for period of time to check all the validations from the APIs

--- src/pages/entry/tests/OrderEntry.test.jsx
+++ src/pages/entry/tests/OrderEntry.test.jsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import OrderEntry from "../OrderEntry";
 import { rest } from "msw";
 import { server } from "../../../mocks/server";
@@ -15,9 +15,9 @@ test("should handles error for scoops and toppings routes", async () => {
 
   render(<OrderEntry />);
 
-  const alerts = await screen.findAllByRole("alert", {
-    name: "An unexpected error occured. Please try again later.",
-  });
+  await waitFor(async () => {
+    const alerts = await screen.findAllByRole("alert");
 
-  expect(alerts).toHaveLength(2);
+    expect(alerts).toHaveLength(2);
+  });
 });
